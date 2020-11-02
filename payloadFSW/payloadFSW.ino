6d053785f7ade5f4cc5939cb14d8b258f1f976c2
MasScout <mas9lol@gmail.com>
2020-11-02 15:28:32 -0600

    Added IMU and Altimeter test code

--- payloadFSW/payloadFSW.ino
+++ payloadFSW/payloadFSW.ino
@@ -1,14 +1,86 @@
 /*
  * Main sketch for Charger Rocket Works Payload Flight Software
  * NASA SLI 2020-21
+ * 
+ * Uses the following libraries:
+ * Adafruit Unified Sensor
+ * Adafruit BNO055
+ * Adafruit BusIO
+ * Adafruit BMP3XX
  */
 
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include "Adafruit_BMP3XX.h>
+#include <utility/imumaths.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+Adafruit_BMP3XX bmp;
+
 void setup() {
-  // put your setup code here, to run once:
-  printf("Hello World");
+  // Initialize 
+  Serial.begin(9600);
+
+  // Initialize IMU
+  if (!bno.begin()){
+    Serial.println("BNO055 Not Detected");
+    while(1);
+  }
+  
+  delay(1000);
+  
+  bno.setExtCrystalUse(true);
+
+
+  
+  // Initialize Altimeter
+  if (!bmp.begin_I2C()) {
+    Serial.println("BMP388 Not Detected");
+    while(1);
+  }
+  
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+  
+ 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Get IMU event
+  sensors_event_t event;
+  bno.getEvent(&event);
 
+  Serial.print("X: ");
+  Serial.print(event.orientation.x, 4);
+  Serial.print("\tY: ");
+  Serial.print(event.orientation.y, 4);
+  Serial.print("\tZ: ");
+  Serial.print(event.orientation.z, 4);
+  Serial.println("");
+
+  if (!bmp.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    return;
+  }
+
+  Serial.print("Temperature = ");
+  Serial.print(bmp.temperature);
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bmp.pressure / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.println();
+  
+  delay(100);
 }
