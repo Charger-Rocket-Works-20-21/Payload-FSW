33c7ea3c8d2bd0562ee994c16bec83eab9419722
MasScout <mas9lol@gmail.com>
2020-11-02 14:45:57 -0600

    Created Framework

---
+++ payloadFSW/payloadFSW.ino
@@ -0,0 +1,14 @@
+/*
+ * Main sketch for Charger Rocket Works Payload Flight Software
+ * NASA SLI 2020-21
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+  printf("Hello World");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
