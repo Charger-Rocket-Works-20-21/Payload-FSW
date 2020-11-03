5d2b769be58901ad18d607f5f9345db798c1d849
Patrick Smith <32267541+PatSmith328@users.noreply.github.com>
2020-11-02 18:45:29 -0600

    Made Initialization Non-Specific

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Payload-FSW
-Flight Software Code for the Payload
+Payload flight software for the 2021 USLI Competition Team from the University of Alabama in Huntsville
+
+Uses:
+* a Teensy 4.1 (with Teensyduino add-on)
+* a Bosch BMP386
+* a Bosch BNO055
+* a NTCLE thermistor
+* a Xbee Radio, and
+* 
 
 Dependencies:
+* Arduino Libraries
 * Adafruit BNO055 Library
 * Adafruit BMP3XX Library
+
+Tasks:
+* Query Positional Sensor
+* Query Pressure Sensor
+* Query Temperature Sensor
+* Calculate Altitude from Pressure and Temperature
+* Detect Landing
+* Level Payload
+* Take Pictures of Landed Surroundings
+* Transmit Telemetry and Photographed Surroundings
--- payloadFSW/payloadFSW.ino
+++ payloadFSW/payloadFSW.ino
@@ -16,7 +16,8 @@
 #include <Adafruit_BMP3XX.h>
 #include <utility/imumaths.h>
 
-#define SEALEVELPRESSURE_HPA (1013.25)
+#define SEALEVELPRESSURE     1013.25   // hPa
+#define SEALEVELTEMPERATRE   273.15    // Kelvin
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 Adafruit_BMP3XX bmp;
@@ -26,27 +27,11 @@ void setup() {
   Serial.begin(9600);
 
   // Initialize IMU
-  if (!bno.begin()){
-    Serial.println("BNO055 Not Detected");
-    while(1);
-  }
-  
-  delay(1000);
-  
-  bno.setExtCrystalUse(true);
-
+  imu_init(&bno);
 
   
   // Initialize Altimeter
-  if (!bmp.begin_I2C()) {
-    Serial.println("BMP388 Not Detected");
-    while(1);
-  }
-  
-  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+  alt_init(&bmp);
   
  
 }
@@ -80,8 +65,37 @@ void loop() {
   Serial.print("Approx. Altitude = ");
   Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
   Serial.println(" m");
-
   Serial.println();
   
   delay(100);
+}
+
+/*
+ * Inertial Measurement Unit Inititialization
+ */
+void imu_init(Adafruit_BNO055* sensor){
+  if (! sensor->begin()){
+    Serial.println("BNO055 Not Detected");
+    while(1);
+  }
+  
+  delay(1000);
+  
+  sensor->setExtCrystalUse(true);
+}
+
+
+/*
+ * Atmospheric Pressure Sensor Inititialization
+ */
+void alt_init(Adafruit_BMP3XX* sensor){
+  if (!sensor->begin_I2C()) {
+    Serial.println("BMP388 Not Detected");
+    while(1);
+  }
+  
+  sensor->setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  sensor->setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  sensor->setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  sensor->setOutputDataRate(BMP3_ODR_50_HZ);
 }
